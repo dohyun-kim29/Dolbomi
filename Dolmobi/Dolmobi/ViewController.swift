17176370958adb0d8004ede429555318e144d9d4
DohyunKIm <rlaehgus1229@gmail.com>
2021-03-15 15:37:22 +0900

    [Add] WebViewTestCode

--- Dolmobi/Dolmobi/ViewController.swift
+++ Dolmobi/Dolmobi/ViewController.swift
@@ -6,14 +6,32 @@
 //
 
 import UIKit
+import WebKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, WKNavigationDelegate {
+    
+    @IBOutlet weak var liveCameraWebView: WKWebView!
+    
+    let camUrl = "http://172.20.10.13:8000"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        request()
+        
     }
+    
+    
+}
 
-
+extension ViewController {
+    
+    
+    func request() {
+        
+        self.liveCameraWebView.load(URLRequest(url: URL(string: camUrl)!))
+        self.liveCameraWebView.navigationDelegate = self
+        
+    }
 }
 
