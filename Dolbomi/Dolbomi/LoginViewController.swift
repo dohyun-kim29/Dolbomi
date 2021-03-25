f2b0d890fd5a2c5b7366a5f5f9b2a11bf6f7bf4e
DohyunKIm <rlaehgus1229@gmail.com>
2021-03-25 11:14:25 +0900

    [Add] SnapKit TestCode

--- Dolbomi/Dolbomi/LoginViewController.swift
+++ Dolbomi/Dolbomi/LoginViewController.swift
@@ -1,17 +1,31 @@
 //
-//  ViewController.swift
+//  LoginViewController.swift
 //  Dolbomi
 //
 //  Created by DohyunKim on 2021/03/15.
 //
 
 import UIKit
+import SnapKit
 
 class LoginViewController: UIViewController {
+    
+    lazy var box = UIView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        makeUI()
+    }
+    
+    func makeUI() {
+        view.backgroundColor = UIColor(named: "DolbomiMainColor")
+        view.addSubview(box)
+        box.backgroundColor = .green
+        box.snp.makeConstraints { con in
+            con.width.height.equalTo(50)
+            con.center.equalTo(self.view)
+        }
     }
 
 
