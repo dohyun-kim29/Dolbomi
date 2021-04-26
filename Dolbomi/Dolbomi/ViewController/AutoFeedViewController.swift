319f13296b9df39a0d7abe1bc2c619355593731d
DohyunKIm <rlaehgus1229@gmail.com>
2021-04-27 08:59:52 +0900

    [Add] AutoFeedViewController

---
+++ Dolbomi/Dolbomi/ViewController/AutoFeedViewController.swift
@@ -0,0 +1,20 @@
+//
+//  AutoFeedViewController.swift
+//  Dolbomi
+//
+//  Created by DohyunKim on 2021/04/27.
+//
+
+import UIKit
+import RxSwift
+import RxCocoa
+import SnapKit
+
+class AutoFeedViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+}
