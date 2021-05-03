8a699cc00aaee899ca68f55ef8f1355be87ec06a
DohyunKIm <rlaehgus1229@gmail.com>
2021-05-03 11:31:45 +0900

    [Add] SettingViewController

---
+++ Dolbomi/Dolbomi/ViewController/SettingViewController.swift
@@ -0,0 +1,34 @@
+//
+//  SettingViewController.swift
+//  Dolbomi
+//
+//  Created by DohyunKim on 2021/05/03.
+//
+
+import UIKit
+import RxSwift
+import RxCocoa
+import SnapKit
+
+class SettingViewController: UIViewController {
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+    }
+    
+    func makeUI() {
+        
+    }
+    
+    func makeConst() {
+        
+    }
+    
+}
